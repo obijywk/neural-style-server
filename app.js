009e35a91a3358bc629ca3fbfbd5d6fa7cd774a1
Matt Gruskin <matthew.gruskin@gmail.com>
2015-11-19 09:38:30 -0500

    image uploads

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
+var bodyParser = require('body-parser');
+var config = require('config');
 var express = require('express');
+var fs = require('fs');
 var morgan = require('morgan');
-var app = express();
+var path = require('path');
 
+try {
+  fs.mkdirSync(config.get('dataPath'));
+} catch (ex) {
+  if (ex.code != 'EEXIST') {
+    throw ex;
+  }
+}
+
+var app = express();
 app.use(morgan('short'));
 
 app.use(express.static('public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+var rawBodyParser = bodyParser.raw({limit: '10mb'});
+app.post('/upload/:id/:purpose', rawBodyParser, function(req, res) {
+  var outputPath = path.join(
+    config.get('dataPath'),
+    req.params.id + '_' + req.params.purpose);
+  fs.writeFile(outputPath, req.body, 'binary', function(err) {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+    res.end();
+  });
+});
 
 var server = app.listen(8000, function() {
   var host = server.address().address;
